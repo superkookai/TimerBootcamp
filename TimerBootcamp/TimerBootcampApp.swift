fae39850e7bf44e7d93793e4ed591906ffa12952
superkookai <weerawut@live.com>
2025-03-14 16:18:34 +0700

    Initial Commit

---
+++ TimerBootcamp/TimerBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerBootcampApp.swift
+//  TimerBootcamp
+//
+//  Created by Weerawut Chaiyasomboon on 14/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct TimerBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
